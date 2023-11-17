31a60cf8d126e75a9c82bf2788d095857bbbd284
Shuvo <shuvoorba28@gmail.com>
2023-11-16 18:06:33 -0800

    context created

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import {
 import './index.css'
 import { router } from './Routes/Routes.jsx';
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './Provider/AuthProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <div className='max-w-screen-2xl mx-auto'>
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className='max-w-screen-2xl mx-auto'>
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
