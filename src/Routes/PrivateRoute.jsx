3c6f2ee0b8c348fba5358184fabe3617c04a2521
Shuvo <shuvoorba28@gmail.com>
2023-11-17 13:12:23 -0800

    loader added for solving classical private route problem

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,10 +1,23 @@
 import { useContext } from "react";
+import { ColorRing } from 'react-loader-spinner'
 import { AuthContext } from "../Provider/AuthProvider";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
 
-    const { user } = useContext(AuthContext)
+    const { user, loading } = useContext(AuthContext);
+
+    if (loading) {
+        return <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+        />
+    }
 
     if (user) {
         return children;
