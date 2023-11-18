f3561d69ad52061de024ffb5d36288ad1ce8260c
Shuvo <shuvoorba28@gmail.com>
2023-11-18 08:21:32 -0800

    tanstack query added

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,11 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
 
 
 const useCart = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+    const axiosSecure = useAxiosSecure(); 
+
+    const {data: cart= []} = useQuery({
+        queryKey: ['cart'], 
+        queryFn: async () => {
+                const res = await axiosSecure.get('/cart')
+                return res.data; 
+        }
+    })
+    return [cart]
 };
 
 export default useCart;
--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,23 @@ import './index.css'
 import { router } from './Routes/Routes.jsx';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './Provider/AuthProvider.jsx';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <HelmetProvider>
-        <div className='max-w-screen-2xl mx-auto'>
-          <RouterProvider router={router} />
-        </div>
-      </HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <div className='max-w-screen-2xl mx-auto'>
+            <RouterProvider router={router} />
+          </div>
+        </HelmetProvider>
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>,
 )
