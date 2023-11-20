17181a183c4bec0db85d949593eec4ec7ffd77e4
Shuvo <shuvoorba28@gmail.com>
2023-11-20 09:04:37 -0800

    functionality added for deleting an user or making an user admin for the admin

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,17 +1,51 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaPeopleArrows, FaPeopleGroup, FaTrash, FaUser, FaUsers } from "react-icons/fa6";
+import Swal from "sweetalert2";
 
 
 const AllUsers = () => {
 
     const axiosSecure = useAxiosSecure();
-    const { data: users = [] } = useQuery({
+    const { data: users = [] , refetch} = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users');
             return res.data;
         }
     })
+
+    const handleDeleteUser = user => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        if (res.data.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                        refetch()
+                    })
+
+            }
+        });
+    }
+
+    const handleMakeAdmin = user => {
+        
+    }
 
     return (
         <div>
@@ -20,23 +54,33 @@ const AllUsers = () => {
                 <h3>Total users: {users.length}</h3>
             </div>
             <div className="overflow-x-auto">
-                <table className="table table-zebra">
+                <table className="table table-zebra w-full">
                     {/* head */}
                     <thead>
                         <tr>
-                            <th></th>
+                            <th>No.</th>
                             <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            users.map(user => <tr key={user._id}>
-                                <th>1</th>
-                                <td>Cy Ganderton</td>
-                                <td>Quality Control Specialist</td>
-                                <td>Blue</td>
+                            users.map((user, index) => <tr key={user._id}>
+                                <th>{index + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    <button onClick={() => handleMakeAdmin(user)} className="btn bg-orange-500 text-white text-2xl btn-outline">
+                                        <FaUsers />
+                                    </button>
+                                </td>
+                                <td>
+                                    <button onClick={() => handleDeleteUser(user)} className="btn btn-outline">
+                                        <FaTrash />
+                                    </button>
+                                </td>
                             </tr>)
                         }
 
