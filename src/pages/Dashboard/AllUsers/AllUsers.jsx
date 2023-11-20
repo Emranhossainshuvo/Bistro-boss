662650f03be635b677034c23152efdd498886733
Shuvo <shuvoorba28@gmail.com>
2023-11-20 08:18:09 -0800

    user's quantity and data get from database

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,9 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
 const AllUsers = () => {
+
+    const axiosSecure = useAxiosSecure(); 
+    const {data : users = []} = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/users'); 
+            return res.data; 
+        }
+    })
+
     return (
         <div>
+            <div className="flex justify-evenly my-4">
             <h3>All users</h3>      
+            <h3>Total users: {users.length}</h3>      
+            </div>
         </div>
     );
 };
