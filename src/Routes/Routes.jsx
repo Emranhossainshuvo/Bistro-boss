089171dee7a3cee4bead5a6000d7ce7ef9e160db
Shuvo <shuvoorba28@gmail.com>
2023-11-21 21:56:59 +0600

    add an item route for admin created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -12,6 +12,8 @@ import PrivateRoute from "./PrivateRoute";
 import Dashboard from "../Layout/Dashboard";
 import Cart from "../pages/Dashboard/Cart/Cart";
 import AllUsers from "../pages/Dashboard/AllUsers/AllUsers";
+import AddItems from "../pages/Dashboard/AddItems/AddItems";
+import AdminRoute from "./AdminRoute";
 
   export const router = createBrowserRouter([
     {
@@ -54,8 +56,12 @@ import AllUsers from "../pages/Dashboard/AllUsers/AllUsers";
         },
         // admin routes
         {
+          path: 'addItems', 
+          element: <AdminRoute><AddItems></AddItems></AdminRoute>
+        },
+        {
           path: 'users', 
-          element: <AllUsers></AllUsers>
+          element: <AdminRoute><AllUsers></AllUsers></AdminRoute>
         }
       ]
     }
---
+++ src/pages/Dashboard/AddItems/AddItems.jsx
@@ -0,0 +1,12 @@
+import SectionTitles from "../../../components/SectionTitles/SectionTitles";
+
+
+const AddItems = () => {
+    return (
+        <div>
+            <SectionTitles subHeading={"---What's new?---"} heading={"Add an item"}></SectionTitles>
+        </div>
+    );
+};
+
+export default AddItems;
