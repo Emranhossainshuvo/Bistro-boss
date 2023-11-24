19d548fe2702128e55af6b4ca820d9cbecad3ccb
Shuvo <shuvoorba28@gmail.com>
2023-11-24 07:50:11 +0600

    payment issues solved and payment amount less then zero problem has been solved

--- src/pages/Dashboard/Payment/CheckOutForm.jsx
+++ src/pages/Dashboard/Payment/CheckOutForm.jsx
@@ -9,68 +9,83 @@ import useAuth from "../../../hooks/useAuth";
 const CheckOutForm = () => {
 
     const [error, setError] = useState('');
-    const [clientSecret, setClientSecret] = useState(''); 
-    const [transactionId, setTransactionId] = useState(''); 
+    const [clientSecret, setClientSecret] = useState('');
+    const [transactionId, setTransactionId] = useState('');
 
-    const stripe = useStripe(); 
+    const stripe = useStripe();
     const elements = useElements();
-    const {user} = useAuth(); 
+    const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
-    const [cart] = useCart(); 
-    const totalPrice = cart.reduce((total, item) => total + item.price , 0)
+    const [cart] = useCart();
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
 
-    useEffect( () => {
-        axiosSecure.post('/create-payment-intent', {price: totalPrice})
-        .then(res => {
-            console.log(res.data.clientSecret); 
-            setClientSecret(res.data.clientSecret); 
-        });
+    useEffect(() => {
+        if (totalPrice) {
+            axiosSecure.post('/create-payment-intent', { price: totalPrice })
+                .then(res => {
+                    console.log(res.data.clientSecret);
+                    setClientSecret(res.data.clientSecret);
+                });
+        }
     }, [axiosSecure, totalPrice])
 
     const handleSubmit = async (event) => {
         event.preventDefault();
         console.log('handle submit button clicked');
-        if(!stripe || !elements){
-            return; 
+        if (!stripe || !elements) {
+            return;
         }
 
-        const card = elements.getElement(CardElement); 
+        const card = elements.getElement(CardElement);
 
-        if(card == null){
-            return; 
+        if (card == null) {
+            return;
         }
 
-        const {error, paymentMethod} = await stripe.createPaymentMethod({
-            type: 'card', 
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
             card
         });
-        if(error){
+        if (error) {
             console.log('payment error', error);
             setError(error.message)
         }
-        else{
-            console.log('payment mathod', paymentMethod); 
+        else {
+            console.log('payment mathod', paymentMethod);
             setError('')
         }
 
         // confirm payment
-        const {paymentIntent, error : confirmError} = await stripe.confirmCardPayment(clientSecret, {
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(clientSecret, {
             payment_method: {
-                card: card, 
+                card: card,
                 billing_details: {
-                    email: user?.email || 'anonymous', 
+                    email: user?.email || 'anonymous',
                     name: user?.displayName || 'anonymous'
                 }
             }
         })
-        if(confirmError){
+        if (confirmError) {
             console.log('confirm error')
         }
-        else{
+        else {
             console.log('payment intent', paymentIntent)
-            if(paymentIntent.status == "succeeded"){
-                console.log('taka mere diyechi', paymentIntent.id);
-                setTransactionId(paymentIntent.id) 
+            if (paymentIntent.status == "succeeded") {
+                console.log('Your transaction Id', paymentIntent.id);
+                setTransactionId(paymentIntent.id)
+
+                const payment = {
+                    email: user.email,
+                    price: totalPrice,
+                    transactionId: paymentIntent.id,
+                    date: new Date(),
+                    cartIds: cart.map(item => item._id),
+                    // ****************************************change name according ot mongodb
+                    menuItemIds: cart.map(item => item.menuId),
+                    status: 'pending'
+                }
+                const res = await axiosSecure.post('/payments', payment);
+                console.log('payment saved', res.data)
             }
         }
     }
@@ -96,7 +111,7 @@ const CheckOutForm = () => {
             />
             <button className="btn btn-secondary btn-sm mt-4" type="submit" disabled={!stripe || !clientSecret}>
                 Pay
-            </button> 
+            </button>
 
             <p className="text-red-600">{error}</p>
             {
