e28e0e58aa566d24e144ec7fd4d788f7a5fa4dda
Shuvo <shuvoorba28@gmail.com>
2023-11-24 10:41:01 +0600

    made some changes and updated node

--- src/pages/Dashboard/PaymentHistory/PaymentHistroy.jsx
+++ src/pages/Dashboard/PaymentHistory/PaymentHistroy.jsx
@@ -1,9 +1,59 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
 const PaymentHistroy = () => {
+
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+
+    const { data: payments = [] } = useQuery({
+        queryKey: ['payments', user],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/payments/${user.email}`);
+            return res.data;
+        }
+    })
     return (
         <div>
-            <h3>This is your payment history</h3>
+            <h3>Total payments: {payments.length}</h3>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        <tr>
+                            <th>1</th>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Blue</td>
+                        </tr>
+                        {/* row 2 */}
+                        <tr>
+                            <th>2</th>
+                            <td>Hart Hagerty</td>
+                            <td>Desktop Support Technician</td>
+                            <td>Purple</td>
+                        </tr>
+                        {/* row 3 */}
+                        <tr>
+                            <th>3</th>
+                            <td>Brice Swyre</td>
+                            <td>Tax Accountant</td>
+                            <td>Red</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
