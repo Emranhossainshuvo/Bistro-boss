f044e878e31e5b7f01d521904b6fe43d36142844
Shuvo <shuvoorba28@gmail.com>
2023-12-04 15:07:30 +0600

    user home route created

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import useAuth from "../../../hooks/useAuth";
 
 const AdminHome = () => {
+
+    const  {user} = useAuth(); 
+
     return (
         <div>
-            
+            <h2 className="text-3xl">Welcome back {
+            user?.displayName ? user.displayName : 'Back'
+            }</h2>
         </div>
     );
 };
